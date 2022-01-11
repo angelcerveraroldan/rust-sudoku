de1a05c111f8f4eaa922063f75fcbc511f1063bf
Angel Cervera Roldan <angelcerveraroldan765@gmail.com>
2022-01-11 14:46:05 +0000

    Added block check algorithm

--- src/board.rs
+++ src/board.rs
@@ -34,8 +34,23 @@ impl Board {
         let mut pos: Vec<u8> = Vec::new();
 
         for n in 0..9 {
+            // Check column
             if self.tiles[row][n].value != 0 && n != col { cannot_be.push(self.tiles[row][n].value) }
+            // Check row
             if self.tiles[n][col].value != 0 && n != row { cannot_be.push(self.tiles[n][col].value) }
+        }
+
+        // Check block
+        let block_index = ((row / 3) as usize, (col / 3) as usize);
+        // Top left tile in that block is
+        let block_top_left = (3 * block_index.0, 3 * block_index.1);
+
+        for i in 0..3 {
+            for j in 0..3 {
+                if self.tiles[block_top_left.0 + i][block_top_left.1 + j].value != 0 {
+                    cannot_be.push(self.tiles[block_top_left.0 + i][block_top_left.1 + j].value)
+                }
+            }
         }
 
         for i in 1..10 {
